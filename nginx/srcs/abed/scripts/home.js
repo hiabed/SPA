9c1a9e3f16bbaf19281b4b02ef9eab73d8049f98
mhassani <mhassani@My-Home.myguest.virtualbox.org>
2024-09-18 11:46:37 +0100

    search bar fetching problem fixed

--- nginx/srcs/abed/scripts/home.js
+++ nginx/srcs/abed/scripts/home.js
@@ -7,7 +7,9 @@ import { chatPage } from "./chat.js";
 import { rankPart } from "./rank.js";
 import { friendsPart, friendsFunction } from "./friends.js";
 
-export const mainFunction = () => {
+let friendsArray;
+export const mainFunction = async () => {
+    friendsArray = await friendsFunction();
     profileId.style.display = "none";
     settingPage.style.display = "none";
     chatPage.style.display = "none";
@@ -26,7 +28,7 @@ let flag = 0;
 let matchBlock = document.createElement("div");
 const homeNav = document.querySelector("#home-navbar");
 
-const lookForUsers = async ()=> {
+const lookForUsers = ()=> {
     document.querySelectorAll(".match-element").forEach(el => {
         el.remove();
     })
@@ -34,8 +36,8 @@ const lookForUsers = async ()=> {
         matchBlock.remove();
         flag = 0;
     }
-    const data = await friendsFunction();
-    data.forEach(element => {
+    const data = friendsArray;
+    friendsArray.forEach(element => {
         if (element.username.includes(searchInput.value.toLowerCase()) && searchInput.value != "") {
             if (flag === 0) {
                 matchBlock.classList.add("match-block");
@@ -43,13 +45,25 @@ const lookForUsers = async ()=> {
                 homeNav.insertAdjacentElement("afterend", matchBlock);
                 flag++;
             }
+            const firstIndex = element.username.indexOf(searchInput.value);
+            const lastIndex = firstIndex + searchInput.value.length;
             const matchElement = document.createElement("div");
             matchElement.classList.add("match-element");
-            matchElement.innerHTML = element.username;
+            const usernameImage = document.createElement("div");
+            usernameImage.style.backgroundImage = `url(${element.imageProfile})`;
+            usernameImage.style.backgroundSize = "cover";
+            usernameImage.style.width = "44px";
+            usernameImage.style.height = "44px";
+            usernameImage.style.border = "white 1px solid";
+            usernameImage.style.borderRadius = "50%";
+            const usenameFound = document.createElement("div");
+            const user = element.username;
+            usenameFound.innerHTML = `${user.substring(0, firstIndex)}<span style="font-weight: bold; color: white;">${user.substring(firstIndex, lastIndex)}</span>${user.substring(lastIndex)}`;
+            matchElement.append(usernameImage, usenameFound);
             matchBlock.append(matchElement);
         }
     });
     // console.log(data);
 }
 
-searchInput.addEventListener("keyup", lookForUsers);
+searchInput.addEventListener("input", lookForUsers);
